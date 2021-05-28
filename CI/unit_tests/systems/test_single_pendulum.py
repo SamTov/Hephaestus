59bb35aadbf3c72e802be7dea8716ae99cc1a175
SamTov <tovey.samuel@gmail.com>
2021-05-28 13:07:59 +0200

    Finish unit tests

--- CI/unit_tests/systems/test_single_pendulum.py
+++ CI/unit_tests/systems/test_single_pendulum.py
@@ -2,30 +2,76 @@
 Test the single pendulum module.
 """
 import unittest
+from hephaestus.systems.single_pendulum import SinglePendulum
 from hephaestus.integrators.euler import Euler
+import numpy as np
 
 
-class TestEuler(unittest.TestCase):
+class TestSimplePendulum(unittest.TestCase):
     """
     Class to test the euler integration module.
     """
     @classmethod
     def setUpClass(cls) -> None:
         """
-        Prepare for the Velocity verlet test.
+        Prepare for the Pendulum class test test.
         """
-        cls.integrator = Euler(time_step=1.0, model=None)
+        cls.pendulum = SinglePendulum(time_step=1,
+                                      steps=100,
+                                      mass=1.0,
+                                      gravity=1.0,
+                                      v0=0.5,
+                                      theta_start=1.0)
 
-    def test_perform_step(self):
+    def test_compute_forces(self):
         """
-        Test the perform step method.
+        Test the compute forced method.
         """
-        state = {'x0': 1.0,
-                 'v0': 0.5,
-                 'a0': 2.0}
-        outcome = self.integrator.perform_step(state)
-        print(outcome['x'])
-        self.assertEqual(outcome, {'x': 3.5})
+        reference_f = -0.29552022
+        forces = self.pendulum.compute_forces(0.3)
+        self.assertEqual(forces, reference_f)
+
+    def test_compute_pe(self):
+        """
+        Test the compute_pe method.
+        """
+        reference_pe = 0.04466349
+        pe = self.pendulum._compute_pe(0.3)
+        self.assertEqual(pe, reference_pe)
+
+    def test_instantiate_system(self):
+        """
+        Test the instantiate system method.
+        """
+        self.assertEqual(self.pendulum.theta[0], np.deg2rad(1.0))
+        self.assertEqual(self.pendulum.velocity[0], np.deg2rad(0.5))
+        self.assertEqual(self.pendulum.force[0],
+                         self.pendulum.compute_forces(np.deg2rad(1.0)))
+        self.assertEqual(self.pendulum.pe[0],
+                         self.pendulum._compute_pe(np.deg2rad(1.0)))
+        self.assertEqual(self.pendulum.ke[0],
+                         self.pendulum._compute_ke(np.deg2rad(0.5)))
+        self.assertIsInstance(self.pendulum.integrator, Euler)
+
+    def test_set_conditions(self):
+        """
+        Test the set conditions method.
+        """
+        reference = {'x0': np.deg2rad(1.0), 'v0': np.deg2rad(0.5), 'a0': self.pendulum.compute_forces(np.deg2rad(1.0))}
+        state = self.pendulum._set_conditions(step=1)
+        self.assertEqual(state, reference)
+
+    def test_compute_velocity(self):
+        """
+        Test the compute velocity method.
+        """
+        self.assertEqual(self.pendulum._compute_velocity(3, 5), 2)
+
+    def test_run_simulation(self):
+        """
+        Test the run simulation method.
+        """
+        self.pendulum.run_simulation()
 
 
 if __name__ == '__main__':
